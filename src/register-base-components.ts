3553c99dfd7c3fa1e30605884cf79e8398d1b293
Nikolay Baskov <baskov@adv.ru>
2021-01-28 12:43:58 +0300

    added async register components

--- src/register-base-components.ts
+++ src/register-base-components.ts
@@ -1,9 +1,10 @@
-import { App } from 'vue';
-
-import BaseInput from '@/components/base/BaseInput.vue';
-import BaseLoader from '@/components/base/BaseLoader.vue';
+import { App, defineAsyncComponent } from 'vue';
 
 export const registerBaseComponents = (app: App) => {
-  app.component('BaseInput', BaseInput);
-  app.component('BaseLoader', BaseLoader);
+  app.component('BaseInput', defineAsyncComponent(
+    () => new Promise((resolve) => resolve(import('@/components/base/BaseInput.vue'))),
+  ));
+  app.component('BaseLoader', defineAsyncComponent(
+    () => new Promise((resolve) => resolve(import('@/components/base/BaseLoader.vue'))),
+  ));
 };
